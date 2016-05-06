bb45dd7fed90f63b5ddd350309c0c83f966c4f98
ANX <anxgang@gmail.com>
2016-05-06 23:06:19 +0800

    Add Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  resources :comments
+
   devise_for :users
   resources :links do
     member do
       get 'upvote'
       get 'downvote'
     end
+    resources :comments
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
