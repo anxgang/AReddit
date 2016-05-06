414ac5f6cb312c9a3e2586fb7b875d1487790de4
ANX <anxgang@gmail.com>
2016-05-06 12:06:14 +0800

    小調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([
+  { email: 'admin@areddit.com', password: '123456', password_confirmation: '123456' }
+])
+
+links = Link.create([
+  { title: 'Google', url: 'http://www.google.com', user_id: 1 },
+  { title: 'Yahoo', url: 'http://www.yahoo.com.tw', user_id: 1 }
+])
